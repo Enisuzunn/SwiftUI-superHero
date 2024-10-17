982d00af98169e8068d1c8c6f6a4ef3f57d2beaa
Enisuzunn <enisbesiktasli@gmail.com>
2024-10-17 18:11:29 +0300

    Initial Commit

---
+++ SwiftUI-superHero/SwiftUI_superHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_superHeroApp.swift
+//  SwiftUI-superHero
+//
+//  Created by Enis Uzun on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_superHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
